eb88df6661f04f7142b3f2ba38767b65fbe29b6a
ChrlB <johnbonaobra106@gmail.com>
2026-08-18 09:24:47 +0800

    add execute Query function on PHPDAO class

--- src/PHPDAO.php
+++ src/PHPDAO.php
@@ -19,8 +19,9 @@ class PHPDAO{
         echo "prepared statement tittled:'$query_tittle' already exists.";
         return;
       };
+      $required_params_count = substr_count($sql_command, '?');
       $pstmt = $this->db_connection->prepare($sql_command);
-      $this->prepared_statements[$query_tittle] = [$pstmt,$description];
+      $this->prepared_statements[$query_tittle] = ["sql"=>$pstmt, "description"=>$description, "params_count" => $required_params_count];
 
     }catch (Exception $e){
       echo $e->getMessage();
@@ -36,9 +37,33 @@ class PHPDAO{
       };
       
       unset($this->prepared_statements[$replacing_query_tittle]);
+      $required_params_count = substr_count($sql_command, '?');
       $pstmt = $this->db_connection->prepare($sql_command);
-      $this->prepared_statements[$query_tittle] = [$pstmt,$description];
+      $this->prepared_statements[$query_tittle] = ["sql"=>$pstmt, "description"=>$description, "params_count" => $required_params_count];
     }catch (Exception $e){
+      echo $e->getMessage();
+      return $e;
+    }
+  }
+
+  public function executeQuery(string $query_tittle, array $params = []){
+    try{
+      $pstmt = $this->prepared_statements[$query_tittle]["sql"];
+      $required_params_count = $this->pstmtParamsCount($query_tittle);
+      $params_count = count($params);
+
+      if(($params_count !== $required_params_count)){
+        echo "Invalid parameter number: number of bound variables does not match number of tokensPDOException Object\n";
+        return;
+      }
+      $success = $pstmt->execute($params); 
+
+      if ($success) {
+          $data = $pstmt->fetchAll(PDO::FETCH_ASSOC); 
+          return $data;
+      }
+
+    }catch(Exception $e){
       echo $e->getMessage();
       return $e;
     }
@@ -46,7 +71,7 @@ class PHPDAO{
 
   public function getPstmtDescription(string $query_tittle): string{
     if(!$this->isPstmtExists( $query_tittle )) return "no prepared statement tittled:'$query_tittle'";
-    return ($this->prepared_statements[$query_tittle][1]);
+    return ($this->prepared_statements[$query_tittle]["description"]);
   }
 
   public function isPstmtExists(string $query_tittle): bool{
@@ -56,6 +81,15 @@ class PHPDAO{
   public function getAllPstmt(): array{
     return $this->prepared_statements;
   }
+
+  public function pstmtParamsCount(string $query_tittle): int{
+    if(!$this->isPstmtExists( $query_tittle )) {
+      echo "no prepared statement tittled:'$query_tittle'";
+      return -1;
+    }
+    return $this->prepared_statements[$query_tittle]["params_count"];
+  }
+
 }
 
 
